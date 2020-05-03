6a56c0a0c945114f83af4bbf3991c0c5f8ecd3d2
Javier <jnnarvaez@uc.cl>
2020-05-03 03:45:04 -0400

    configuraciones par keroku

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import HamburguesaList, HamburguesaDetail, IngredienteList, IngredienteDetail, consulta_anidada, api_root
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
