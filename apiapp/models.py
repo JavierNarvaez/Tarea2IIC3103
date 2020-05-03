f38c6ae191ca6fd41e4021c8b00dd2832612b2f8
Javier <jnnarvaez@uc.cl>
2020-05-03 16:36:18 -0400

    campos obligatorios

--- apiapp/models.py
+++ apiapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Ingrediente(models.Model):
-    nombre = models.CharField(max_length=100, default='')
-    descripcion = models.CharField(max_length=100, default='')
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
 
 #    def __str__(self): #cambiar para poder hacer la anidación y/o que imprima ingredientes = [{path: url}]
 #        path = 'http://localhost:8000/ingrediente/' ####CAMBIAAAAAAR
@@ -13,10 +13,10 @@ class Ingrediente(models.Model):
 
 class Hamburguesa(models.Model):
 
-    nombre = models.CharField(max_length=100, default='')
+    nombre = models.CharField(max_length=100)
     precio = models.IntegerField()
-    descripcion = models.CharField(max_length=100,  default='')
-    imagen = models.CharField(max_length=100, default='')
+    descripcion = models.CharField(max_length=100)
+    imagen = models.CharField(max_length=100)
     ingredientes = models.ManyToManyField(Ingrediente, blank=True, related_name='hamburguesas')
 
    
